d1d8a5a87c27351f0a468886844a660d7a793894
Ayush Srivastava <ayush625sri@gmail.com>
2024-11-24 12:31:26 +0530

    Added login API

--- src/profiles_project/profiles_api/urls.py
+++ src/profiles_project/profiles_api/urls.py
@@ -6,6 +6,7 @@ from .views import *
 router = DefaultRouter()
 router.register('hello-viewset', HelloViewSet, basename='hello-viewset')
 router.register('profile', UserProfileViewSet)
+router.register('login', LoginViewSet, basename='login')
 urlpatterns = [
     path('hello-view/', HelloApiView.as_view()),
     path('', include(router.urls))
--- src/profiles_project/profiles_api/views.py
+++ src/profiles_project/profiles_api/views.py
@@ -4,6 +4,9 @@ from rest_framework.response import Response
 from rest_framework import viewsets
 from rest_framework import status
 from rest_framework.authentication import TokenAuthentication
+from rest_framework.authtoken.serializers import AuthTokenSerializer
+from rest_framework.authtoken.views import ObtainAuthToken 
+from rest_framework.authtoken.models import Token 
 from rest_framework import filters
 
 from .serializers import *
@@ -120,4 +123,14 @@ class UserProfileViewSet(viewsets.ModelViewSet):
     filter_backends = (filters.SearchFilter,)
     search_fields = ('name', 'email',)
 
+class LoginViewSet(viewsets.ViewSet):
+    """Checks email and password and returns ann auth token."""
+
+    serializer_class = AuthTokenSerializer
+
+    def create(self, request):
+        """Use the ObtainAuthToken APIVIew to validate and create a token."""
+
+        # return ObtainAuthToken().post(request)
+        return ObtainAuthToken().as_view()(request=request._request)
 
